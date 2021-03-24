fe137461ee614f95c3f378926dbb2487de012ec7
Hyo Woo You <nonhyo6902@gmail.com>
2021-03-24 23:17:39 +0900

    21.03.24.php.ninja

---
+++ 2021.03.24_php_Ninja.php
@@ -0,0 +1,9 @@
+<?php 
+    $pdg = new PDO('mysql:host=localhost; dbname=ijdb','nonhyo','jhs1741jhs@@');
+?>
+<?php 
+    $pdg = new PDO('mysql:host=호스트명; 
+                    dbname=DB명',
+                    '사용자명',
+                    '비밀번호');
+?>
